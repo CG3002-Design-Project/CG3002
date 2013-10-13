793355be28e48677766d453a2bd411ef19f02a87
EC2 Default User <ec2-user@ip-172-31-14-204.ap-southeast-1.compute.internal>
2013-10-13 08:07:42 +0000

    removed django folder

--- HQProject/products/views.py
+++ HQProject/products/views.py
@@ -3,11 +3,12 @@
 from django.http import HttpResponse
 from django.template import Context, loader, RequestContext
 from django.shortcuts import render_to_response
-from products.models import Products
+from products.models import Products,Store
 
 
 def search_form(request):
-    return render_to_response('search_form.html',{}, context_instance=RequestContext(request))
+    stores = Store.objects.all()
+    return render_to_response('search_form.html',{'stores':stores}, context_instance=RequestContext(request))
 		   
 
 def search(request):
