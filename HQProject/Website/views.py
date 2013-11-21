2714f5b34d77b9a6bef159d79ebd776982316ee6
dipikasuresh <dipika.s92@gmail.com>
2013-11-21 15:23:25 +0800

    updated HQ

--- HQProject/Website/views.py
+++ HQProject/Website/views.py
@@ -17,16 +17,10 @@ def filter_products(request):
    
    
 def view_product(request):
-    if 'cate' in request.GET and request.GET['cate']:
-        category = request.GET['cate']
-        if category == 'All':
-            products = Product.objects.all()
-            c = 'all products'
-        else:
-            products = Product.objects.filter(category=category)
-            c = category
-        context = {'products':products,'c':c}
-        return render(request, 'view_product.html',context)
+    products = Product.objects.all()    
+    context = {'products':products}
+	print products
+    return render(request,'view_product.html',context)
 
 		
 def add_product(request):
@@ -61,41 +55,21 @@ def product_added(request):
 		
 		
 def view_storewise(request,id):
+    product = Product.objects.get(product_id_id=id)
     inventory = Inventory.objects.filter(product_id_id=id)
-    product = Product.objects.get(product_id=id)
+    storeids = []
+    for i in inventory:     
+        if i.store_id not in storeids:
+            storeids.append(i.store_id)
     stores = []
-    for b in batches:
-        s = Store.objects.get(store_id=b.store_id_id)
-        if s not in stores:
-            stores.append(s)
-    class s_b:
-        def __init__(self):
-            self.address = None	
-            self.country = None
-            self.region = None
-            self.minimum_qty = 0
-            self.selling_price = 0
-            self.qty = 0
-            self.expiry_date = None
-            self.batch_id = None
-            self.store_id = None
-    sb = []
-    for (s,b) in zip(stores,batches):  
-        if s.id == b.store_id_id:
-            x = s_b()
-            x.address = s.address + ' ' + s.city + ' ' + s.state
-            x.country = s.country
-            x.region = s.region 			
-            x.minimum_qty = b.minimum_qty
-            x.selling_price = b.selling_price
-            x.qty = b.qty
-            x.expiry_date = b.expiry_date
-            x.batch_id = b.batch_id
-            x.store_id = b.store_id_id
-            sb.append(x)			
-    context = {'sb':sb,'product':product}
+    for s in storeids:
+        st = Store.objects.get(store_id=s)
+        stores.append(st)
+    list = zip(store, inventory)		
+    context = {'list':list,'product':product}
     return render(request, 'view_storewise.html',context)
-
+	
+	
 def filter_stores(request):
    return render(request,'filter_stores.html')
    
