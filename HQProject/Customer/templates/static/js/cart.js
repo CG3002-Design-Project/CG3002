d42d8321b87f7e5a817e6b97f897a3adbe97311d
Poornima-Muthukumar <poo.dav@gmail.com>
2013-12-08 03:50:23 +0800

    poornima's update

---
+++ HQProject/Customer/templates/static/js/cart.js
@@ -0,0 +1,22 @@
+var cart_list = [];
+var store_selected = 0;
+var total = 0;
+
+window.onload = function() {
+	init();
+}
+
+function init(){
+	$('#add_to_cart').click(function(){
+		console.log("here");
+	//if(el.className == "on") {
+		//el.className="off";
+		//el.value = "Add to cart"
+	//} else {
+		//console.log("here");
+		//var id = $(this).closest("tr").find(".nr").text();
+		//alert(id)
+		//el.className="on";
+		//el.value = "added"
+	}
+}
