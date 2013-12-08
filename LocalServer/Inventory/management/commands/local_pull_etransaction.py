742209f7099683e74272748a9c19b5fd39720751
dipikasuresh <dipika.s92@gmail.com>
2013-12-08 18:18:58 +0800

    new commands

--- LocalServer/Inventory/management/commands/local_pull_etransaction.py
+++ LocalServer/Inventory/management/commands/local_pull_etransaction.py
@@ -1,10 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.management.base import BaseCommand, CommandError
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import Context, loader, RequestContext
 from Inventory.models import Inventory
 from Inventory.models import Product
-from Inventory.models import Transaction
+from Inventory.models import Transaction, eTransaction
 import datetime
 from decimal import *
 from django.views.decorators.csrf import csrf_exempt
@@ -15,7 +18,7 @@ import time
 import serial
 #from restock import *
 
-shopid = 00000005
+shopid = 00000001
 cashid = 00001
 
 hq_host_inventory = "http://ec2-user@ec2-54-254-157-48.ap-southeast-1.compute.amazonaws.com:8000/Sync/processInventory"
@@ -24,33 +27,29 @@ hq_host_request = "http://ec2-user@ec2-54-254-157-48.ap-southeast-1.compute.amaz
 hq_host_product = "http://ec2-user@ec2-54-254-157-48.ap-southeast-1.compute.amazonaws.com:8000/Sync/processProduct"
 hq_host_product_pull = "http://ec2-user@ec2-54-254-157-48.ap-southeast-1.compute.amazonaws.com:8000/Sync/receiveProduct"
 hq_host_inventory_pull = "http://ec2-user@ec2-54-254-157-48.ap-southeast-1.compute.amazonaws.com:8000/Sync/receiveInventory"
-hq_host_etransaction_pull = "http://ec2-user@ec2-54-254-157-48.ap-southeast-1.compute.amazonaws.com:8000/Sync/receiveETransaction"
+hq_host_e_pull = "http://ec2-user@ec2-54-254-157-48.ap-southeast-1.compute.amazonaws.com:8000/Sync/localPullETransaction"
 
 class Command(BaseCommand):
-    help = 'Local pull of eTransactions'
-    def handle(self, **options):
-        print "entered pull from hq"
-	payload = {'shopid': shopid }
-	r = requests.get(hq_host_etransaction_pull,params=payload)
-	data = json.loads(r.text)
-	trans = data['trans']
-	for t in trans:
-		transaction_id = int(t['transaction_id'])
-		transaction_date = t['transaction_date']
-		product_id = int(t['product_id'])
-		batch_id = int(t['batch_id'])
-		qty = int(t['qty'])
-		cost_price = Decimal(t['cost_price'])
-		selling_price = Decimal(t['selling_price'])
-		status = t['status']		
-		et = eTransaction(transaction_id=transaction_id,transaction_date=transaction_date,product_id=product_id,batch_id=batch_id,quantity_sold = qty,selling_price=selling_price,cost_price=cost_price,status=status) 
-		et.save()
-		inv = Inventory.objects.get(product_id_id=product_id,batch_id=batch_id)
-		inv.qty = inv.qty - qty
-		inv.save()		
-# To set this up:
-# 1. Create file pricing_strategy_cronjob
-# 2. In the file, add below line 
-# 0 0 * * 0-6 python manage.py recalculate_price 
-# (min hr day month day_run Command) << File format, this job runs recalculate_price.py every day at 12am
-# 3. On Unix system, run this to set it up: crontab -a pricing_strategy_cronjob                 
+	help = 'Local pull of eTrans'
+	def handle(self, **options):
+		print "entered pull from hq"
+	
+		payload = {'shopid': shopid }
+		r = requests.get(hq_host_e_pull,params=payload)
+		data = json.loads(r.text)
+		etran = data['etran']
+		for t in etran:
+			transaction_id = int(t['transaction_id'])
+			transaction_date = t['transaction_date']
+			product_id = int(t['product_id'])
+			batch_id = int(t['batch_id'])
+			qty = int(t['quantity_sold'])
+			cost_price = Decimal(t['cost_price'])
+			selling_price = Decimal(t['selling_price'])
+			status = t['status']		
+			et = eTransaction(transaction_id=transaction_id,transaction_date=transaction_date,product_id=product_id,batch_id=batch_id,quantity_sold = qty,selling_price=selling_price,cost_price=cost_price,status=status) 
+			et.save()
+			inv = Inventory.objects.get(product_id_id=product_id,batch_id=batch_id)
+			inv.qty = inv.qty - qty
+			inv.save()		
+	
--- LocalServer/Inventory/management/commands/local_pull_inventory.py
+++ LocalServer/Inventory/management/commands/local_pull_inventory.py
@@ -15,7 +15,7 @@ import time
 import serial
 #from restock import *
 
-shopid = 00000005
+shopid = 00000001
 cashid = 00001
 
 hq_host_inventory = "http://ec2-user@ec2-54-254-157-48.ap-southeast-1.compute.amazonaws.com:8000/Sync/processInventory"
@@ -30,47 +30,49 @@ class Command(BaseCommand):
     help = 'Local pull of Inventory'
 
     def handle(self, **options):
-        print "entered pull from hq"
-        payload = {'shopid': shopid }
-        r = requests.get(hq_host_inventory_pull,params=payload)
-        data = json.loads(r.text)
-        invent = data['inventory']
-        for i in invent:
-            product_id = int(i['productid'])
-            batch_id = int(i['batchid'])
-            qty = int(i['qty'])
-            cost_price = Decimal(i['cost_price'])
-            expiry_date = i['expiry_date']
-            selling_price = Decimal(i['selling_price'])
-            minimum_qty = int(i['minimum_qty'])
-            
-            inventor = Inventory.objects.filter(product_id=product_id, batch_id=batch_id) 
+		print "entered pull from hq"
+		payload = {'shopid': shopid }
+		r = requests.get(hq_host_inventory_pull,params=payload)
+		data = json.loads(r.text)
+		invent = data['inventory']
+		for i in invent:
+			product_id = int(i['productid'])
+			batch_id = int(i['batchid'])
+			qty = int(i['qty'])
+			cost_price = Decimal(i['cost_price'])
+			expiry_date = i['expiry_date']
+			selling_price = Decimal(i['selling_price'])
+			minimum_qty = int(i['minimum_qty'])
+			
+			inventor = Inventory.objects.filter(product_id_id=product_id, batch_id=batch_id) 
 
-            for inventory in inventor:
-                if(inventory is not None): 
-                    inventory.product_id_id = product_id
-                    inventory.batch_id = batch_id
-                    inventory.qty = qty
-                    inventory.cost_price = cost_price
-                    if(expiry_date != 'None'):
-                        expiry_date = datetime.datetime.strptime(i['expiry_date'], '%Y-%m-%d').date()
-                        inventory.expiry_date = expiry_date
-                    inventory.selling_price = selling_price
-                    inventory.minimum_qty = minimum_qty
-                    inventory.save();
-                else:
-                    if(expiry_date != 'None'):
-                        expiry_date = datetime.datetime.strptime(i['expiry_date'], '%Y-%m-%d').date()
-                        inventory.expiry_date = expiry_date
-                    inventory_new = Inventory(product_id_id = product_id,
-                                          batch_id = batch_id,
-                                          qty = qty,
-                                          cost_price = cost_price,
-                                          expiry_date = expiry_date,
-                                          selling_price = selling_price,
-                                          minimum_qty = minimum_qty);
-                
-                    inventory_new.save()            
+			for inventory in inventor:
+				if(inventory is not None): 
+					inventory.product_id_id = product_id
+					inventory.batch_id = batch_id
+					inventory.qty = qty
+					inventory.cost_price = cost_price
+					if(expiry_date != 'None'):
+						expiry_date = datetime.datetime.strptime(i['expiry_date'], '%Y-%m-%d').date()
+						inventory.expiry_date = expiry_date
+					inventory.selling_price = selling_price
+					inventory.minimum_qty = minimum_qty
+					inventory.save();
+				else:
+					if(expiry_date != 'None'):
+						expiry_date = datetime.datetime.strptime(i['expiry_date'], '%Y-%m-%d').date()
+						inventory.expiry_date = expiry_date
+					inventory_new = Inventory(product_id_id = product_id,
+										  batch_id = batch_id,
+										  qty = qty,
+										  cost_price = cost_price,
+										  expiry_date = expiry_date,
+										  selling_price = selling_price,
+										  minimum_qty = minimum_qty);
+				
+					inventory_new.save()
+		return HttpResponse('inventory pull')
+		#return render(request,'sync_function.html');
 
 # To set this up:
 # 1. Create file pricing_strategy_cronjob
